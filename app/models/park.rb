9fe66a0e615f9bb296e5746e947e0624f7c492d2
David Groulx <david@sandbendersoftware.com>
2012-10-27 17:32:26 -0400

    Added park links back in, passwords working again

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,9 +1,5 @@
 class Park < ActiveRecord::Base
-<<<<<<< HEAD
-  attr_accessible :address, :contact_info, :flickr_pool, :history, :latitude, :longitude, :name, :park_size, :section, :vimeo_embed, :amenity_ids, :slug
-=======
-  attr_accessible :address, :contact_info, :flickr_pool, :history, :latitude, :longitude, :name, :park_size, :section, :vimeo_embed, :amenity_ids, :slug
->>>>>>> c13d60676a48516add725ef0d07caa9b348c25ab
+  attr_accessible :address, :contact_info, :flickr_pool, :history, :latitude, :longitude, :name, :park_size, :vimeo_embed, :amenity_ids, :slug
 
   has_many :park_amenities
   has_many :amenities, through: :park_amenities
