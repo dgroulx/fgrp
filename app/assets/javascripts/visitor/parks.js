7ff98166b6df842f00dcb57ec1af3306b9cb3144
Jeremy Bell <grcodemonkey@gmail.com>
2012-10-27 18:10:46 -0400

    Added geolocation search capabilities

--- app/assets/javascripts/visitor/parks.js
+++ app/assets/javascripts/visitor/parks.js
@@ -1,77 +1,96 @@
-$(document).ready(function() {
+$(function () {
 
-	var markers = [];
-	var $canvas = $('#map_canvas');
-	var map;
+    var map,
+        markers = [],
+        $canvas = $('#map_canvas');
 
-	var refreshParkList = function(data) {
-		var existingMarkers = $canvas.data('markers');
-		while (existingMarkers && existingMarkers.length > 0) {
-			existingMarkers.pop().setMap(null);
-		}
+    var refreshParkList = function (data) {
+        var existingMarkers = $canvas.data('markers');
+        while (existingMarkers && existingMarkers.length > 0) {
+            existingMarkers.pop().setMap(null);
+        }
 
-		if (! (data instanceof Array)) {
-			data = [data];
-		}
+        if (!(data instanceof Array)) {
+            data = [data];
+        }
 
-		$.each(data, function(i, park) {
-			//console.log(park.name + ": " + park.latitude + ", " + park.longitude);
-			var marker = new google.maps.Marker({
-				position : new google.maps.LatLng(park.latitude, park.longitude),
-				map : map,
-				animation : google.maps.Animation.DROP,
-				title : park.name
-			});
-			google.maps.event.addListener(marker, 'click', function() {
-				location.href = "/parks/" + park.slug;
-			});
-			markers.push(marker);
-		});
-		$canvas.data('markers', markers);
-		$('.parkCount').html(markers.length);
+        $.each(data, function (i, park) {
+            //console.log(park.name + ": " + park.latitude + ", " + park.longitude);
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(park.latitude, park.longitude),
+                map: map,
+                animation: google.maps.Animation.DROP,
+                title: park.name
+            });
+            google.maps.event.addListener(marker, 'click', function () {
+                location.href = "/parks/" + park.slug;
+            });
+            markers.push(marker);
+        });
+        $canvas.data('markers', markers);
+        $('.parkCount').html(markers.length);
+    }
+
+    var buildMap = function (lat, long, zoom, url) {
+        var mapOptions = {
+            center: new google.maps.LatLng(lat, long), // grand rapids lat/long
+            zoom: zoom,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            disableDefaultUI: true
+        };
+        map = new google.maps.Map($canvas[0], mapOptions);
+
+        $.getJSON(url, refreshParkList);
+    };
+
+    $('.search-input').on('change refresh', function (e) {
+        var $form = $('#search-form'),
+            $distanceSearch = $('.distance-search'),
+            address = $('#address').val();
+
+        if (!address) {
+            $distanceSearch.prop('disabled', true);
+        }
+
+        var data = $form.serialize();
+
+        $distanceSearch.prop('disabled', false);
+
+        $.getJSON('/parks.json', data, refreshParkList);
+    });
+
+    $('#amenity_id').chosen({ no_results_text: "No results matched" });
+
+    $('#address').on('change', function () {
+        var geoCoder = new google.maps.Geocoder(),
+            $address = $(this);
+        geoCoder.geocode({ address: $address.val(), bounds: map.getBounds() }, function (results, status) {
+            if (status === google.maps.GeocoderStatus.OK && results && results.length > 0) {
+                var address = results[0],
+                    location = address.geometry.location;
+                map.setCenter(location);
+                $('#lat').val(location.lat());
+                $('#lng').val(location.lng());
+                $address.val(address.formatted_address);
+                $('.search-input').trigger('refresh');
+            }
+        });
+    });
+
+prettify();
+
+    if ($("#park_slug").length > 0) {
+        var resource = '/parks/' + $("#park_slug").val() + '.json';
+        var lat = $("#park_lat").val();
+        var lng = $("#park_long").val();
+        buildMap(lat, lng, 15, resource);
+    } else {
+        buildMap(42.9633, -85.6681, 12, '/parks.json');
+    }
+    
+    function prettify(){
+	  for(i=0;i<4;i++){
+	    $('aside').append('<span class="concave-corner"></span>')
+	  }
 	}
-
-	var buildMap = function(lat, long, zoom, url) {
-		var mapOptions = {
-			center : new google.maps.LatLng(lat, long), // grand rapids lat/long
-			zoom : zoom,
-			mapTypeId : google.maps.MapTypeId.ROADMAP,
-			disableDefaultUI : true
-		};
-		map = new google.maps.Map($canvas[0], mapOptions);
-
-		$.getJSON(url, refreshParkList);
-	};
-
-	$('select.amenities').chosen({no_results_text: "No results matched"}).change(function(e) {
-		var selected = $(this).val();
-		if (selected && selected.length > 0) {
-			var resource = 'parks.json?';
-			$.each(selected, function(i) {
-				if (i > 0) resource += '&';
-				resource += 'amenity_id=' + this;
-			});
-		}
-		$.getJSON(resource, refreshParkList);
-	});
-
-	if ($("#park_slug").val()) {
-		var resource = '/parks/' + $("#park_slug").val()  + '.json';
-		var lat = $("#park_lat").val();
-		var lng = $("#park_long").val();
-		buildMap(lat, lng, 15, resource);
-	} else {
-		buildMap(42.9633, -85.6681, 12, 'parks.json');
-	}
-
-	prettify();
-
 });
-
-
-
-function prettify(){
-  for(i=0;i<4;i++){
-    $('aside').append('<span class="concave-corner"></span>')
-  }
-}
--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,13 +1,24 @@
 class ParksController < ApplicationController
   def index
     @amenities = Amenity.all
+	@allParks = Park.all
     if params[:amenity_id].present?
       @parks = Park.joins(:amenities).where("amenities.id = ?", params[:amenity_id])
     else 
-      @parks = Park.all
+      @parks = @allParks
     end
+  
+  if params[:distance].present?
+   distance = params[:distance].to_f
+   lat = params[:lat].to_f
+   lng = params[:lng].to_f
+   @parks = @parks.select{ |x| haversine_distance(lat, lng, x.latitude, x.longitude) < distance }
+  end
+
     @facts = Fact.find(Fact.pluck(:id).sample(4))
   
+  puts "@parks => " + @parks.to_s
+
     respond_to do |format|
       format.html
       format.js
@@ -24,4 +35,33 @@ class ParksController < ApplicationController
       format.json { render json: @park.to_json }
     end
   end
+
+  private
+
+  # PI = 3.1415926535
+	RAD_PER_DEG = 0.017453293  #  PI/180
+
+	Rmiles = 3956           # radius of the great circle in miles
+
+	def haversine_distance( lat1, lon1, lat2, lon2 )
+
+		dlon = lon2 - lon1
+		dlat = lat2 - lat1
+
+		dlon_rad = dlon * RAD_PER_DEG 
+		dlat_rad = dlat * RAD_PER_DEG
+
+		lat1_rad = lat1 * RAD_PER_DEG
+		lon1_rad = lon1 * RAD_PER_DEG
+
+		lat2_rad = lat2 * RAD_PER_DEG
+		lon2_rad = lon2 * RAD_PER_DEG
+
+		a = (Math.sin(dlat_rad/2))**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad) * (Math.sin(dlon_rad/2))**2
+		c = 2 * Math.atan2( Math.sqrt(a), Math.sqrt(1-a))
+
+		dMi = Rmiles * c          # delta between the two points in miles
+	end
+
+
 end
