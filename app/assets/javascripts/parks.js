b9bfee706e1d07bb9518ae2b820e31d0ba340ee6
Karen Morand <karen.wasielewski@cengage.com>
2012-10-26 23:05:51 -0400

    add map with multiple markers to parks page

--- app/assets/javascripts/parks.js
+++ app/assets/javascripts/parks.js
@@ -1,3 +1,33 @@
-$(document).ready( function() {
-  $.getScript("/parks/" + $("#park_slug").val() + ".js"); 
+$(document).ready(function() {
+
+	// TODO: remove me
+	$.getScript("/parks/" + $("#park_slug").val() + ".js");
+
+	var grLatLong = new google.maps.LatLng(42.9633, -85.6681);
+	var mapOptions = {
+		center : grLatLong,
+		zoom : 12,
+		mapTypeId : google.maps.MapTypeId.ROADMAP,
+		disableDefaultUI : true
+	};
+
+	var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+	var markers = [];
+
+	$.getJSON('parks.json', function(data) {
+		$.each(data, function(i, park) {
+			console.log(park.name + ": " + park.latitude + ", " + park.longitude);
+			var marker = new google.maps.Marker({
+				position : new google.maps.LatLng(park.latitude, park.longitude),
+				map : map,
+				animation : google.maps.Animation.DROP,
+				title : park.name
+			});
+			google.maps.event.addListener(marker, 'click', function() {
+				location.href = "/parks/" + park.slug;
+			});
+			markers.push(marker);
+		});
+	});
+
 });
