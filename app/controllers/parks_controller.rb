efe70d340665a8dfae97a219d7c3fe7fc7a2881c
Jeremy Bell <grcodemonkey@gmail.com>
2012-10-27 00:16:00 -0400

    Added query to parks index action

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,6 +1,10 @@
 class ParksController < ApplicationController
   def index
+  if params[:amenity_id].present?
+    @parks = Park.joins(:amenities).where("amenities.id = ?", params[:amenity_id])
+  else 
     @parks = Park.all
+  end
 
     respond_to do |format|
       format.html
@@ -16,6 +20,14 @@ class ParksController < ApplicationController
       format.html
       format.js
       format.json { render json: @park.to_json }
+    end
+  end
+
+  def find
+	@parks = Park.joins(:amenities).where("amenities.id = ?", params[:amenity_id])
+
+	respond_to do |format|
+      format.json { render json: @parks.to_json }
     end
   end
 end
