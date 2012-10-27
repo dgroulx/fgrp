3b34fb8889ef61738ae4f89233d1042abef8d4e2
Jeremy Bell <grcodemonkey@gmail.com>
2012-10-26 22:02:42 -0400

    Added data to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Park.create([
+	{name: "Ah-Nab-Awen", latitude: 42.9683, longitude: -85.6767, address: "220 Front Ave NW Grand Rapids"},
+	{name: "Aberdeen Park", latitude: 43.0045, longitude: -85.6497, address: "Eastern at Evelyn NE Grand Rapids"},
+	{name: "Highland Park", latitude: 42.9754, longitude: -85.657, address: "700 College NE Grand Rapids"},
+	]);
