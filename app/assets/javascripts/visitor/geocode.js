401d2959b07a255d7214305984b6bdd6b491de49
Jeremy Bell <grcodemonkey@gmail.com>
2012-10-27 11:26:02 -0400

    Added geocoder sample

---
+++ app/assets/javascripts/visitor/geocode.js
@@ -0,0 +1,11 @@
+﻿var geoCoder = new google.maps.Geocoder();
+
+geoCoder.geocode({ address: '38 West Fulton, Grand Rapids, MI' }, function (results, status) {
+    if (status === 'OK' && results && results.length > 0) {
+        var address = results[0],
+            location = address.geometry.location;
+        alert(address.formatted_address);
+        alert(location.lat());
+        alert(location.lng());
+    }
+});
