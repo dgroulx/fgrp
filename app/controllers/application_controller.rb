4ec579d9186740ec7f710d814394ce9bb9401573
David Groulx <david@sandbendersoftware.com>
2012-10-28 11:47:53 -0400

    route changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,9 @@ class ApplicationController < ActionController::Base
   # Overwriting the sign_out redirect path method
   def after_sign_out_path_for(resource_or_scope)
     admin_root_path
-  end  
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    admin_root_path
+  end
 end
