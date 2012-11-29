9bc5f5f5085a7253f56a3ecce8becf456d90a275
David Groulx <david@sandbendersoftware.com>
2012-11-29 17:17:03 -0500

    fixed copy example config files deploy set

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ namespace :deploy do
   task :setup_config, roles: :app do
     run "mkdir -p #{shared_path}/config"
     put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
-    put File.read("config/application.example.yml"), "#{shared_path}/config/database.yml"
+    put File.read("config/application.example.yml"), "#{shared_path}/config/application.yml"
     puts "Now edit the config files in #{shared_path}"
   end
   after "deploy:setup", "deploy:setup_config"
