5c12c908a7d0074f9d5e9bd449f4dcc07e93c52d
David Groulx <david@sandbendersoftware.com>
2012-12-02 21:01:37 -0500

    typo in symlink task

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,7 +40,7 @@ namespace :deploy do
 
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-    run "ln -nfs #{shared_path}/config/application.yml #{release_path}/config/application.yml}"
+    run "ln -nfs #{shared_path}/config/application.yml #{release_path}/config/application.yml"
   end
 
   desc "Make sure local git is in sync with remote."
