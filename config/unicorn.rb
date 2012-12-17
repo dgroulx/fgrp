b28a075ed7013081c3bb90facb7aeff55bf6d352
David Groulx <david@sandbendersoftware.com>
2012-12-17 15:30:10 -0500

    Revert "Removed nginx and unicorn config"
    This reverts commit 50579f076befb09e20211824bdc7bedfe647c864.

---
+++ config/unicorn.rb
@@ -0,0 +1,9 @@
+root = "/home/fgrp_vps/explore/current"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.explore.sock"
+worker_processes 2
+timeout 30
