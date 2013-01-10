9f0cba3757b8c46170ab4c8a59c9a5120db6a610
David Groulx <david@sandbendersoftware.com>
2013-01-10 15:39:53 -0500

    Disable assets pipeline

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,7 @@ module Fgrp
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    config.assets.enabled = false
 
     config.assets.initialize_on_precompile = false
 
