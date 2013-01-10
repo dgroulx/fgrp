fc6c6b7e9c49c4f6514e9cb840e66703db1d7a6b
David Groulx <david@sandbendersoftware.com>
2013-01-10 13:17:00 -0500

    Expire all assets to try and get them to recompile in deployment

--- config/application.rb
+++ config/application.rb
@@ -61,7 +61,7 @@ module Fgrp
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
 
     # Custom manifest files
     config.assets.precompile += ['admin.js', 'visitor.js']
